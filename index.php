5ca50cddae6acda3ce51337764a3cd03a4e18878
Isaac Rovira <isaac.rovira.rocasalbas@gmail.com>
2020-11-15 16:21:55 +0100

    Actualización de las rutas. -Añadido Modules/routes.php -index.php importa routes.php

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php
+$path = '\repos\UOC\3SCode\Producto2\Modules';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+$path = '\repos\UOC\3SCode\Producto2\Public';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+// Include router class
+include('router.php');
 
-$req = parse_url($_SERVER['REQUEST_URI']);
-echo implode($req);
-echo "NNNNNNNNNNN";
-echo ($_SERVER['REQUEST_URI']);
+// Add base route (startpage)
+Route::add('/',function(){
+    echo 'Welcome :-)';
+});
 
-switch($req){
-    case '/' :
-    case '':
-        echo $req;
-    case '/prueba':
-        echo 'prueba';
-        echo $req;
-        echo $_SERVER['REQUEST_URI'];
-    break;
-}
+// Simple test route that simulates static html file
+Route::add('/test',function(){
+	require 'test.php';
+    #echo 'Hello from test.php';
+});
 
+// Post route example
+Route::add('/contact-form',function(){
+    echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+},'get');
 
+// Post route example
+Route::add('/contact-form',function(){
+    echo 'Hey! The form has been sent:<br/>';
+    print_r($_POST);
+},'post');
+
+// Accept only numbers as parameter. Other characters will result in a 404 error
+Route::add('/foo/([0-9]*)/bar',function($var1){
+    echo $var1.' is a great number!';
+});
+
+Route::run('/');
+
+/*<!--https://steampixel.de/en/simple-and-elegant-url-routing-with-php/-->*/
 ?>
